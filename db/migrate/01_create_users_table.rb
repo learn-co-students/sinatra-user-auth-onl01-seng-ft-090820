07c199338a8aa0ec0d4f00804fb6d8c264b456fd
dashalary <dk2800@nyu.edu>
2020-10-15 18:25:06 -0400

    Done.

---
+++ db/migrate/01_create_users_table.rb
@@ -0,0 +1,11 @@
+class CreateUsersTable < ActiveRecord::Migration
+
+    def change
+        create_table :users do |user|
+          user.string :name
+          user.string :email
+          user.string :password
+        end
+      end
+
+end
